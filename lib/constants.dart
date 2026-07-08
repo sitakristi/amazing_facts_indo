24c0b23a0001bdf4ae8b274ae7cc58d2163fc9d0
RasitaKris <rulliyani@student.ciputra.ac.id>
2026-07-08 11:42:20 +0700

    Update ALP

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,4 @@
 class AppConfig {
-  // Kembali menggunakan IP Wi-Fi lokal rumah kamu
-  static const String baseUrl = 'http://192.168.18.36:8080/api';
-  static const String authBaseUrl = 'http://192.168.18.36:8080/api'; 
+  static const String baseUrl = 'http://localhost:8080/api';
+  static const String authBaseUrl = 'http://localhost:8080/api'; 
 }
